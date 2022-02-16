74c933ffe56981a19a2bb99fe8a8b367844debe2
B-NightShade <79291516+B-NightShade@users.noreply.github.com>
2022-02-16 15:27:16 -0700

    no error checking but working

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.TreeMap;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
 import java.util.Map;
 
 public class Main {
